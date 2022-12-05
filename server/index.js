805bb7206ce6eee9a2debd224b58b49196497e75
Anand Pap <anandpap@live.com>
2022-12-05 04:30:32 +0100

    test commit to vercel branch

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+//asd
 
 const app = express();
 
