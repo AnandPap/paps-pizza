0f7263530cbb86f5bc221dbeebe75bfdb2bce129
Anand Pap <anandpap@live.com>
2022-12-07 20:34:12 +0100

    asd

--- server/index.js
+++ server/index.js
@@ -41,8 +41,7 @@ app.listen(config.port, (err) => {
 });
 
 app.use(express.static(path.join(__dirname, "../client", "dist")));
-app.get("/*", (req, res) => {
-  res.send(5);
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
 });
 
