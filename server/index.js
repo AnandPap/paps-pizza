c837cf18179ed4113b76be529bb93cb00dcc0259
Anand Pap <anandpap@live.com>
2022-12-08 20:03:49 +0100

    asd

--- server/index.js
+++ server/index.js
@@ -46,5 +46,5 @@ mongoose
 
 app.use(express.static(path.join(__dirname, "client", "dist")));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
