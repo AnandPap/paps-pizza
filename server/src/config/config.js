e1816cf490c89cd35536803fec550c7d893c889a
Anand Pap <anandpap@live.com>
2022-12-08 14:19:15 +0100

    deploying client without server

--- server/src/config/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const config = {
-  port: process.env.PORT || 5000,
-  secret: process.env.JWT_SECRET || "secretkey",
-  mongo: process.env.MONGO || `mongodb://localhost:27017`,
-};
-
-module.exports = config;
