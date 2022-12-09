3d15129478408b09149f995c0594c7f04f17f0e2
Anand Pap <anandpap@live.com>
2022-12-09 06:58:17 +0100

    some more personal testing

--- client/src/MainRouter.jsx
+++ client/src/MainRouter.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const MainRouter = () => {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />}>
