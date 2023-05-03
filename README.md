0bc6234771f499ee56a2ece1c3d44637a819bd71
Anand Pap <anandpap@live.com>
2023-05-03 18:13:00 +0200

    fix

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Pap's Pizza
 
-### This version is example of full MERN app deployed on Vercel:
+### This version is an example of full MERN app deployed on vercel.com:
 
-#### "master" branch deployed live version: https://vercel-paps-pizza-anandpap.vercel.app/ (ctrl + click to open in new tab)
+#### "master" branch deployed live version: https://paps-pizza-vercel-full.vercel.app/ (ctrl + click to open in new tab)
 
-### While this one on the other hand contains just frontend while server is deployed on render.com site:
+### While this one, on the other hand, contains just frontend while server is deployed on render.com:
 
-#### "render.com" branch deployed live version: https://paps-pizza-anandpap.vercel.app/ (ctrl + click to open in new tab)
+#### "render.com" branch deployed live version: https://paps-pizza-render.vercel.app/ (ctrl + click to open in new tab)
--- server/index.js
+++ server/index.js
@@ -1,36 +1,26 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const compress = require("compression");
 const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
 const config = require("./src/config/config");
 const userRoutes = require("./src/routes/user.routes");
-const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(express.json()); //activated body parse in our code
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({ origin: "https://paps-pizza-anandpap.vercel.app/", credentials: true })
-);
 app.use(compress());
 app.use(helmet());
-//   {
-//   contentSecurityPolicy: {
-//     //blocks my login and register attempts if I don't use it
-//     useDefaults: true,
-//     directives: {
-//       "connect-src": ["'self'", "http://localhost:5000"],
-//       "style-src": [
-//         "'self'",
-//         "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css",
-//       ],
-//     },
-//   },
-// }
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://paps-pizza-vercel-full.vercel.app/",
+    credentials: true,
+  })
+);
 
 app.use(userRoutes);
 
