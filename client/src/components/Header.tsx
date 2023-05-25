59379869ed1aaf2a2394282370cc7bf2fa2956c4
Anand Pap <anandpap@live.com>
2023-05-25 15:04:01 +0200

    new build files

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,52 +12,50 @@ const Header = () => {
   const dispatch = useAppDispatch();
 
   return (
-    <>
-      <header className="header">
-        <div className="logo-and-title-container">
-          <div
-            onClick={() => history("/", { replace: true })}
-            className="logo-and-title-wrapper"
-          >
-            <img className="logo" src={pizza} alt="" />
-            <h2 className="title">Pap's Pizza</h2>
+    <header className="header">
+      <div className="logo-and-title-container">
+        <div
+          onClick={() => history("/", { replace: true })}
+          className="logo-and-title-wrapper"
+        >
+          <img className="logo" src={pizza} alt="" />
+          <h2 className="title">Pap's Pizza</h2>
+        </div>
+      </div>
+      {pizzasPicked.length > 0 ? (
+        <i className="bi bi-cart-fill cart-icon"></i>
+      ) : (
+        <i className="bi bi-cart4 cart-icon"></i>
+      )}
+      {isLoggedIn ? (
+        <div className="dropdown">
+          <i className="bi bi-person-circle avatar"></i>
+          <div className="dropdown-content">
+            {/* <p onClick={() => history("/orderhistory")}>Order History</p> */}
+            <p
+              onClick={() => {
+                clearJWT();
+                dispatch(setIsLoggedIn(false));
+                history("/");
+                dispatch(resetPizzas());
+              }}
+            >
+              Sign out
+            </p>
           </div>
         </div>
-        {pizzasPicked.length > 0 ? (
-          <i className="bi bi-cart-fill cart-icon"></i>
-        ) : (
-          <i className="bi bi-cart4 cart-icon"></i>
-        )}
-        {isLoggedIn ? (
-          <div className="dropdown">
-            <i className="bi bi-person-circle avatar"></i>
-            <div className="dropdown-content">
-              {/* <p onClick={() => history("/orderhistory")}>Order History</p> */}
-              <p
-                onClick={() => {
-                  clearJWT();
-                  dispatch(setIsLoggedIn(false));
-                  history("/");
-                  dispatch(resetPizzas());
-                }}
-              >
-                Sign out
-              </p>
-            </div>
-          </div>
-        ) : (
-          <Button
-            text="Log In"
-            className="signin-button"
-            onClick={() => {
-              dispatch(setShowLogInModal(true));
-              history("/login");
-            }}
-          />
-        )}
-        <hr />
-      </header>
-    </>
+      ) : (
+        <Button
+          text="Log In"
+          className="signin-button"
+          onClick={() => {
+            dispatch(setShowLogInModal(true));
+            history("/login");
+          }}
+        />
+      )}
+      <hr />
+    </header>
   );
 };
 
