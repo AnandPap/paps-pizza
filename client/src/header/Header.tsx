3535ad8635c74698315dc08db9a32fa675784531
Anand Pap <anandpap@live.com>
2023-07-06 04:05:14 +0200

    refactored signout logic in header component and introduced notification box to it

--- client/src/header/Header.tsx
+++ client/src/header/Header.tsx
@@ -1,26 +1,70 @@
-import { useState } from "react";
 import Button from "../reusable/Button";
 import pizza from "../assets/images/pizza2.png";
 import { useAppSelector, useAppDispatch } from "../redux/hooks";
-import { setIsLoggedIn, setPizza } from "../redux/pizza";
+import {
+  setIsLoggedIn,
+  setPizza,
+  setUsername,
+  setNotification,
+  closeNotification,
+} from "../redux/pizza";
 import { useNavigate } from "react-router-dom";
 import { signout } from "../helpers/fetch-functions";
 import Spinner from "../reusable/Spinner";
 import { getErrorMessage } from "../helpers/error-functions";
+import NotificationBox from "../reusable/NotificationBox";
 
 const Header = () => {
-  const [error, setError] = useState("");
-  const { pizzasPicked, isLoggedIn, username } = useAppSelector((s) => s.pizza);
+  const { pizzasPicked, isLoggedIn, username, notification } = useAppSelector(
+    (s) => s.pizza
+  );
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
+  async function signoutHandler() {
+    try {
+      const res = await signout();
+      if (res && "message" in res) {
+        location.pathname !== "/" && navigate("/", { replace: true });
+        dispatch(setIsLoggedIn(false));
+        dispatch(setUsername(""));
+        dispatch(setPizza({ type: "reset" }));
+        dispatch(
+          setNotification({
+            text: res.message,
+            type: "success",
+          })
+        );
+      } else {
+        dispatch(
+          setNotification({
+            text: getErrorMessage(res),
+            type: "error",
+          })
+        );
+      }
+      setTimeout(() => dispatch(closeNotification()), 1500);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function navigateHandler(destination: string) {
+    location.pathname !== destination && navigate(destination);
+  }
+
   return (
     <header>
-      <div onClick={() => location.pathname !== "/" && navigate("/")}>
-        <img src={pizza} alt="" />
+      <div onClick={() => navigateHandler("/")}>
+        <img src={pizza} alt="Pizza logo" />
         <h1>Pap's Pizza</h1>
       </div>
       <div>
+        <NotificationBox
+          text={notification.text}
+          show={notification.show}
+          type={notification.type}
+        />
         {pizzasPicked.length > 0 ? (
           <i className="bi bi-cart-fill cart-icon" title="Cart full"></i>
         ) : (
@@ -30,32 +74,13 @@ const Header = () => {
           <div className="dropdown">
             <i className="bi bi-person-circle avatar" />
             <div className="dropdown-content">
-              <p onClick={() => navigate(`user-profile/${username}`)}>
+              <p onClick={() => navigateHandler(`/user-profile/${username}`)}>
                 {username}
               </p>
-              <p
-                onClick={() =>
-                  location.pathname !== "/order-history" &&
-                  navigate("/order-history")
-                }
-              >
+              <p onClick={() => navigateHandler("/order-history")}>
                 Order History
               </p>
-              <p
-                onClick={() => {
-                  signout()
-                    .then((res) => {
-                      if (res === true) {
-                        location.pathname !== "/" && navigate("/");
-                        dispatch(setIsLoggedIn(false));
-                        dispatch(setPizza({ type: "reset" }));
-                      } else setError(getErrorMessage(res));
-                    })
-                    .catch((err) => console.log(err));
-                }}
-              >
-                Sign out
-              </p>
+              <p onClick={signoutHandler}>Sign out</p>
             </div>
           </div>
         ) : isLoggedIn === false ? (
